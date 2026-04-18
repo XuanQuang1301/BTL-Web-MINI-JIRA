32683694863748428f1d483e7281780cf156817b
cuongcuongdt20-ui <cuongcuongdt20@gmail.com>
2026-04-18 21:27:03 +0700

    Add files via upload

---
+++ backend/src/main/java/com/web/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
